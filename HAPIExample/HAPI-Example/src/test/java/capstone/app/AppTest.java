f9ce01b471f16fe1ba63cd8201cc573c6363a161
Spencer Diernbach <stsld05@moravian.edu>
2016-02-26 14:50:09 -0500

    Updated the test file from Junit3 to Junit4

--- HAPIExample/HAPI-Example/src/test/java/capstone/app/AppTest.java
+++ HAPIExample/HAPI-Example/src/test/java/capstone/app/AppTest.java
@@ -1,36 +1,17 @@
 package capstone.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+        /**
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
         assertTrue( true );
